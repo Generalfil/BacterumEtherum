2c6e8185659fc7788e207de2eeab87175cc03cae
Generalfil <fille.anders@hotmail.com>
2018-09-27 11:23:58 +0200

    Added base stuff

--- BacterumEtherum/Assets/Scripts/BacteriaController.cs
+++ BacterumEtherum/Assets/Scripts/BacteriaController.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class BacteriaController : MonoBehaviour {
 
+	private Vector3[,] BacPos;
+
+	public int GridSize = 50;
+
 	// Use this for initialization
 	void Start () {
-		
+		BacPos = new Vector3[GridSize, GridSize];
 	}
 	
 	// Update is called once per frame
---
+++ BacterumEtherum/Assets/Scripts/BaseBacteria.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts
+{
+	public class BaseBacteria
+	{
+		protected int Health;
+		protected float Growchance;
+		protected int Attack;
+
+		protected void CheckAdjecentPos()
+		{
+
+		}
+
+		protected void AttackOther()
+		{ }
+
+		protected void Grow()
+		{ }
+
+		protected void Mutate()
+		{ }
+
+		protected void Die()
+		{ }
+
+	}
+}
