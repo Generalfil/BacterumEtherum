a73d348f99d140270c7857bcbe047f1f09022b31
Generalfil <fille.anders@hotmail.com>
2018-11-07 16:06:04 +0100

    Started work on functionality

--- BacterumEtherum/Assets/Scripts/BacteriaController.cs
+++ BacterumEtherum/Assets/Scripts/BacteriaController.cs
@@ -1,20 +1,50 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BacteriaController : MonoBehaviour {
+namespace Assets.Scripts
+{
+	public class BacteriaController : MonoBehaviour
+	{
+		private BaseBacteria[,] BacPos;
+		private List<BaseBacteria> BacteriaList;
+		private float TickTime;
 
-	private Vector3[,] BacPos;
+		public int GridSize = 50;
 
-	public int GridSize = 50;
+		// Use this for initialization
+		void Start()
+		{
+			BacPos = new BaseBacteria[GridSize, GridSize];
+			BacteriaList = new List<BaseBacteria>();
+			InititializePositions(BacPos);
+			TickTime = 0f;
+		}
 
-	// Use this for initialization
-	void Start () {
-		BacPos = new Vector3[GridSize, GridSize];
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+		// Update is called once per frame
+		void Update()
+		{
+			
+		}
+
+		private void FixedUpdate()
+		{
+			TickTime += Time.deltaTime;
+			if (TickTime >= 0.25f)
+			{
+
+			}
+		}
+
+
+		private void InititializePositions(BaseBacteria[,] _bacPos)
+		{
+			_bacPos[0, 25] = new BaseBacteria();
+			_bacPos[25, 0] = new BaseBacteria();
+			_bacPos[50, 25] = new BaseBacteria();
+			_bacPos[25, 50] = new BaseBacteria();
+		}
+
 	}
 }
--- BacterumEtherum/Assets/Scripts/BaseBacteria.cs
+++ BacterumEtherum/Assets/Scripts/BaseBacteria.cs
@@ -2,19 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
 
 namespace Assets.Scripts
 {
-	public class BaseBacteria
+	public class BaseBacteria : MonoBehaviour
 	{
-		protected int Health;
+		System.Random random;
+		protected float Health;
 		protected float Growchance;
-		protected int Attack;
+		protected float Attack;
+		public Vector3 Position;
+
+		
+		public BaseBacteria(): base()
+		{
+			random = new System.Random();
+			float healthMod = CalculateHealthModifier(3,10);
+
+			this.Health = 10f + healthMod;
+			this.Growchance = 0.1f;
+			this.Attack = 1f;
+		}
+
+		public BaseBacteria(float health, float growChance, float attack)
+		{
+			random = new System.Random();
+			float healthMod = CalculateHealthModifier(3, 10);
+
+			this.Health = health + healthMod;
+			this.Growchance = growChance;
+			this.Attack = attack;
+		}
 
 		protected void CheckAdjecentPos()
-		{
-
-		}
+		{ }
 
 		protected void AttackOther()
 		{ }
@@ -27,6 +49,18 @@ namespace Assets.Scripts
 
 		protected void Die()
 		{ }
+
+		private float CalculateHealthModifier(int loops, int numberToRandom)
+		{
+			float healthMod = 0;
+			for (int i = 0; i < loops; i++)
+			{
+				healthMod += random.Next(numberToRandom);
+			}
+
+			healthMod /= 3;
+			return healthMod;
+		}
 
 	}
 }
