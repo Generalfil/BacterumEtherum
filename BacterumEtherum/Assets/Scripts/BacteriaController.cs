847f8abf84ef89f00fcb88ba5842863d1808f604
Generalfil <fille.anders@hotmail.com>
2018-11-20 15:54:22 +0100

    Kinda working bas project

--- BacterumEtherum/Assets/Scripts/BacteriaController.cs
+++ BacterumEtherum/Assets/Scripts/BacteriaController.cs
@@ -2,23 +2,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Threading;
 
 namespace Assets.Scripts
 {
 	public class BacteriaController : MonoBehaviour
 	{
 		private BaseBacteria[,] BacPos;
-		private List<BaseBacteria> BacteriaList;
-		private float TickTime;
+		private List<GameObject> BacteriaList;
+		private bool InitDone = false;
+		public float TickTime;
 
-		public int GridSize = 50;
+		public int GridSize = 30;
 
 		// Use this for initialization
 		void Start()
 		{
 			BacPos = new BaseBacteria[GridSize, GridSize];
-			BacteriaList = new List<BaseBacteria>();
-			InititializePositions(BacPos);
+			BacteriaList = new List<GameObject>();
+			InititializePositions();
 			TickTime = 0f;
 		}
 
@@ -30,21 +32,75 @@ namespace Assets.Scripts
 
 		private void FixedUpdate()
 		{
-			TickTime += Time.deltaTime;
-			if (TickTime >= 0.25f)
+			if (InitDone)
 			{
-
+				TickTime += Time.deltaTime;
+				if (TickTime >= 0.5f)
+				{
+					Tick();
+					TickTime = 0;
+				}
 			}
+			
 		}
 
-
-		private void InititializePositions(BaseBacteria[,] _bacPos)
+		private void Tick()
 		{
-			_bacPos[0, 25] = new BaseBacteria();
-			_bacPos[25, 0] = new BaseBacteria();
-			_bacPos[50, 25] = new BaseBacteria();
-			_bacPos[25, 50] = new BaseBacteria();
+			var bacList = BacteriaList;
+			Co1();
 		}
 
+		IEnumerator Co1()
+		{
+			foreach (var bac in BacteriaList)
+			{
+				List<GameObject> baseBacterias = bac.GetComponent<BaseBacteria>().CheckAdjecentPos();
+				Co2(bac, baseBacterias);
+				//Check for empty
+				List<Vector3> emptyPos = bac.GetComponent<BaseBacteria>().CheckEmptyAdjecentPos();
+				Co3(bac, emptyPos);
+			}
+			return null;
+		}
+
+		IEnumerator Co3(GameObject bac, List<Vector3> emptyPos)
+		{
+			foreach (var posToGrow in emptyPos)
+			{
+				if (bac.GetComponent<BaseBacteria>().Grow())
+					CreateBacteria(posToGrow);
+			}
+			return null;
+		}
+
+		IEnumerator Co2(GameObject bac, List<GameObject> baseBacterias)
+		{
+			foreach (var posToAttack in baseBacterias)
+			{
+				if (posToAttack != null)
+					bac.GetComponent<BaseBacteria>().AttackOther(posToAttack.gameObject);
+			}
+			return null;
+		}
+
+		private void InititializePositions()
+		{
+			Vector3[] vector3s = new Vector3[] {new Vector3(0,0,15), new Vector3(15, 0, 0), new Vector3(15, 0, 30), new Vector3(30, 0, 15), };
+
+			foreach (var v3 in vector3s)
+			{
+				CreateBacteria(v3);
+			}
+			InitDone = true;
+		}
+
+		private void CreateBacteria(Vector3 v3)
+		{
+			GameObject BacteriaObj = GameObject.CreatePrimitive(PrimitiveType.Cube);
+			BacteriaObj.AddComponent<BaseBacteria>().Position = v3;
+			BacteriaObj.GetComponent<BaseBacteria>().SetCheckArr();
+			BacteriaObj.transform.position = v3;
+			BacteriaList.Add(BacteriaObj);
+		}
 	}
 }
--- BacterumEtherum/Assets/Scripts/BaseBacteria.cs
+++ BacterumEtherum/Assets/Scripts/BaseBacteria.cs
@@ -8,13 +8,16 @@ namespace Assets.Scripts
 {
 	public class BaseBacteria : MonoBehaviour
 	{
-		System.Random random;
+		
 		protected float Health;
 		protected float Growchance;
 		protected float Attack;
 		public Vector3 Position;
+		public Guid Guid;
 
-		
+		private System.Random random;
+		private List<Vector3> CheckArr;
+
 		public BaseBacteria(): base()
 		{
 			random = new System.Random();
@@ -23,9 +26,11 @@ namespace Assets.Scripts
 			this.Health = 10f + healthMod;
 			this.Growchance = 0.1f;
 			this.Attack = 1f;
+			this.Guid = Guid.NewGuid();
+			Debug.Log("I was born: " + Guid);
 		}
 
-		public BaseBacteria(float health, float growChance, float attack)
+		public BaseBacteria(float health, float growChance, float attack, Guid guid)
 		{
 			random = new System.Random();
 			float healthMod = CalculateHealthModifier(3, 10);
@@ -33,16 +38,50 @@ namespace Assets.Scripts
 			this.Health = health + healthMod;
 			this.Growchance = growChance;
 			this.Attack = attack;
+			this.Guid = guid;
 		}
 
-		protected void CheckAdjecentPos()
+		public List<GameObject> CheckAdjecentPos()
+		{
+			var AdjacentBac = new List<GameObject>();
+			
+			foreach (var chPos in CheckArr)
+			{
+				var Bac = FindAt(chPos);
+				AdjacentBac.Add(Bac);
+			}
+
+			return AdjacentBac;
+		}
+		public List<Vector3> CheckEmptyAdjecentPos()
+		{
+			var AdjacentBac = new List<Vector3>();
+
+			foreach (var chPos in CheckArr.Where(pos => pos.x <= 30 && pos.x >= 0 && pos.z <= 30 && pos.z >= 0))
+			{
+				var Bac = FindAt(chPos);
+				if(Bac == null)
+					AdjacentBac.Add(chPos);
+			}
+
+			return AdjacentBac;
+		}
+
+		public void AttackOther(GameObject m_gameObject)
 		{ }
 
-		protected void AttackOther()
-		{ }
-
-		protected void Grow()
-		{ }
+		public bool Grow()
+		{
+			if (random.Next(1, 100) >= 100*Growchance)
+			{
+				Debug.Log("Growing");
+				return true;
+			}
+			else
+			{
+				return false;
+			}	
+		}
 
 		protected void Mutate()
 		{ }
@@ -61,6 +100,34 @@ namespace Assets.Scripts
 			healthMod /= 3;
 			return healthMod;
 		}
+
+		private GameObject FindAt(Vector3 pos)
+		{
+			pos.y += 1; // shift the position 1 unit above
+			RaycastHit hit;
+
+			// cast a ray downwards with range = 2
+			if (Physics.Raycast(pos, Vector3.down, out hit, 2))
+				return hit.collider.gameObject;
+			else
+				return null;
+		}
+
+		public void SetCheckArr()
+		{
+			CheckArr = new List<Vector3>()
+			{
+				new Vector3(Position.x, Position.y, Position.z + 1),
+				new Vector3(Position.x, Position.y, Position.z - 1),
+				new Vector3(Position.x + 1, Position.y, Position.z),
+				new Vector3(Position.x - 1, Position.y, Position.z),
+				new Vector3(Position.x + 1, Position.y, Position.z + 1),
+				new Vector3(Position.x - 1, Position.y, Position.z + 1),
+				new Vector3(Position.x + 1, Position.y, Position.z - 1),
+				new Vector3(Position.x - 1, Position.y, Position.z - 1)
+			};
+		}
+ 
 
 	}
 }
